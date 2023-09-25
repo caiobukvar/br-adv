e167309f8194cd7ea5142ec621d597efb2a870d2
Caio <bukvar2009@gmail.com>
2023-09-25 16:47:40 -0300

    global bg color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <p>Home</p>
-      <a href="/quem-somos">Quem somos</a>
-      <a href="/blog">Blog</a>
-      <a href="/contato">Contato</a>
+      <h1>Início</h1>
+      <div className={styles.nav}>
+        <a href="/quem-somos">Quem somos</a>
+        <a href="/blog">Blog</a>
+        <a href="/contato">Contato</a>
+      </div>
     </main>
   )
 }
