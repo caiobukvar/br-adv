7257042e4a3dc33adbe4e4ea38ed7ae909c4ba53
Caio <bukvar2009@gmail.com>
2023-09-27 16:30:27 -0300

    adding new font - testing

--- src/components/UnderConstruction/index.tsx
+++ src/components/UnderConstruction/index.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import Construction from "../../assets/images/construction/under-construction.svg";
 import styles from "./styles.module.css";
+import { Inter, Teko } from "next/font/google";
+const teko = Teko({ subsets: ["latin"] });
 
 export default function UnderConstruction() {
   return (
-    <div className={styles.construction}>
-      <Image src={Construction} alt="construction" />
-      <h1>Página em construção!</h1>
+    <div className={teko.className}>
+      <div className={styles.construction}>
+        <Image src={Construction} alt="construction" />
+        <h1>Página em construção!</h1>
+      </div>
     </div>
   );
 }
