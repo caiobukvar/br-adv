0e4992c695c2b8843e905cf6e16b80612ed44674
Caio <bukvar2009@gmail.com>
2023-09-29 08:45:23 -0300

    base building - section 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
+import Image from "next/image";
 import styles from "./page.module.css";
 import UnderConstruction from "@/components/UnderConstruction";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <UnderConstruction />
+      {/* <UnderConstruction /> */}
+      <section className={styles["section__1"]}>
+        <div>
+          <h1>Escritório de advocacia BR</h1>
+          <div className={styles["s1__subtitle"]}>
+            <h3>DIREITO IMOBILIÁRIO</h3>
+          </div>
+          <button>Saiba mais</button>
+        </div>
+        <Image src="" alt="img-s1" width={400} height={400} />
+      </section>
+      
       <div className={styles.nav}>
         <a href="/">Início</a>
         <a href="/quem-somos">Quem somos</a>
