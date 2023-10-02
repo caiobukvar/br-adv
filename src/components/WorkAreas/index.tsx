4fb0466c74ae6ef74a5e4cfceb5349ed81616d39
Caio <bukvar2009@gmail.com>
2023-10-02 17:47:27 -0300

    adding whatsapp button

--- src/components/WorkAreas/index.tsx
+++ src/components/WorkAreas/index.tsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
-import Construction from "../../assets/images/construction/under-construction.svg";
 import styles from "./styles.module.css";
 import { Inter, Aleo } from "next/font/google";
-import DImob from "../../../public/work-area-cards/direito-imobiliario.jpg";
-import DIfam from "../../../public/work-area-cards/direito-de-familia.jpg";
+import WhatsappIcon from "../../assets/images/icons/logo-whatsapp-white.svg";
+import WhatsappIconGreen from "../../assets/images/icons/logo-whatsapp-green.svg";
 
 const teko = Aleo({ subsets: ["latin"] });
 
@@ -70,20 +69,38 @@ export default function WorkAreas() {
     <div className={teko.className}>
       <div className={styles.container}>
         {specs.map((spec, index) => (
-          <div
-            key={spec.id}
-            className={index % 2 === 0 ? styles.even : styles.odd}
-            id={`${index}`}
-            style={{ backgroundImage: `url(${spec.img})` }}
-          >
-            <div className={styles.list}>
-              <h3>{spec.title}</h3>
-              <ul>
-                {spec.items.map((item) => (
-                  <li key={item}>{item}</li>
-                ))}
-              </ul>
+          <div key={spec.id} className={styles.wrapper}>
+            <div
+              className={index % 2 === 0 ? styles.even : styles.odd}
+              id={`${index}`}
+              style={{ backgroundImage: `url(${spec.img})` }}
+            >
+              <div className={styles.list}>
+                <h3>{spec.title}</h3>
+                <ul>
+                  {spec.items.map((item) => (
+                    <li key={item}>{item}</li>
+                  ))}
+                </ul>
+              </div>
             </div>
+            <button className={styles.btn__whatsapp}>
+              <Image
+                src={WhatsappIcon}
+                alt="icon-whats-white"
+                width={20}
+                height={20}
+                className={styles.image__white}
+              />
+              <Image
+                src={WhatsappIconGreen}
+                alt="icon-whats"
+                width={20}
+                height={20}
+                className={styles.image__green}
+              />
+              <p>Contato</p>
+            </button>
           </div>
         ))}
       </div>
