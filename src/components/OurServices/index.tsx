17fda44f8f21e8d3325e5eeaec59014440e96e6e
Caio <bukvar2009@gmail.com>
2023-10-03 06:08:10 -0300

    adding json on separate file - services

--- src/components/OurServices/index.tsx
+++ src/components/OurServices/index.tsx
@@ -1,23 +1,6 @@
 import Image from "next/image";
 import styles from "./styles.module.css";
-
-const services = [
-  {
-    name: "Advocacia extrajudicial",
-    text: "Bianca Rocha Advocacia é um escritório completo, sempre em busca da solução rápida para as questões, economizando o tempo do cliente e aumentando a satisfação do cliente",
-    img: "",
-  },
-  {
-    name: "Direito de família e sucessões",
-    text: "Abordagem personalizada em cada caso, experiência na atuação em divórcios judiciais e extrajudiciais, inventários, processos de guarda, etc.",
-    img: "",
-  },
-  {
-    name: "Consultorias",
-    text: "Consultoria especializada em processos envolvendo direito imobiliário, com elaboração e revisão de contratos, análise de risco de compra e venda, usucapião, ações possessórias, etc.",
-    img: "",
-  },
-];
+import services from "../../services.json";
 
 export default function OurServices() {
   return (
