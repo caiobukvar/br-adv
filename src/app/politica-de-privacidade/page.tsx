ab8a0bab04a3cdfcb0f3ea818cf4ec2e480a13d3
Caio <bukvar2009@gmail.com>
2023-10-05 11:44:43 -0300

    fix em construcao

--- src/app/politica-de-privacidade/page.tsx
+++ src/app/politica-de-privacidade/page.tsx
@@ -1,5 +1,11 @@
+import UnderConstruction from "@/components/UnderConstruction";
 import styles from "./page.module.css";
 
 export default function Politicas() {
-  return <div>Politica de privacidade</div>;
+  return (
+    <div>
+      <p>Politica de privacidade</p>
+      <UnderConstruction />
+    </div>
+  );
 }
--- src/app/quem-somos/page.tsx
+++ src/app/quem-somos/page.tsx
@@ -5,12 +5,6 @@ export default function QuemSomos() {
   return (
     <main className={styles.main}>
       <UnderConstruction />
-      <div className={styles.nav}>
-        <a href="/">Início</a>
-        <a href="/quem-somos">Quem somos</a>
-        <a href="/contato">Contato</a>
-        <a href="/blog">Blog</a>
-      </div>
     </main>
   );
 }
