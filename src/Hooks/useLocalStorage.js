7538fb1a6a609b63abeaab5ec007c1ec0ae3fbb2
Caio <bukvar2009@gmail.com>
2023-10-19 16:17:56 -0300

    push test

---
+++ src/Hooks/useLocalStorage.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+export function useLocalStorage(key, defaultValue) {
+  const [value, setValue] = useState(
+    localStorage.getItem(key) || defaultValue
+  );
+
+  useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [key, value]);
+
+  return [value, setValue];
+}
