5802f8ee37813167a28520ddc49ecf752b691c51
Caio <bukvar2009@gmail.com>
2023-10-19 16:46:33 -0300

    adding window.

--- src/Hooks/useLocalStorage.js
+++ src/Hooks/useLocalStorage.js
@@ -2,10 +2,10 @@
 import { useState, useEffect } from "react";
 
 export function useLocalStorage(key, defaultValue) {
-  const [value, setValue] = useState(localStorage.getItem(key) || defaultValue);
+  const [value, setValue] = useState(window.localStorage.getItem(key) || defaultValue);
 
   useEffect(() => {
-    localStorage.setItem(key, value);
+    window.localStorage.setItem(key, value);
   }, [key, value]);
 
   return [value, setValue];
