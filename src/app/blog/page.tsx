1fe021d1f49ae580a94b0d7bfc38cfdd79ec667f
Caio <bukvar2009@gmail.com>
2023-10-26 17:47:05 -0300

    adding metadata wherever possible

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,12 @@
 import styles from "./page.module.css";
 import BlogPosts from "@/components/BlogPosts";
 
+export async function generateMetadata() {
+  return {
+    title: "Blog",
+  };
+}
+
 export default function Blog() {
   return (
     <main className={styles.main}>
