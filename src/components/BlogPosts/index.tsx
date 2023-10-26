53c18d5b2d9b98ccdeee81a89daaa90881c6c5b2
Caio <bukvar2009@gmail.com>
2023-10-26 10:16:13 -0300

    responsiveness added to blog section

--- src/components/BlogPosts/index.tsx
+++ src/components/BlogPosts/index.tsx
@@ -2,13 +2,14 @@ import styles from "./styles.module.css";
 import blogPosts from "../../blogPosts.json";
 import Link from "next/link";
 import { VStack } from "@chakra-ui/react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
 
 export default function BlogPosts() {
   return (
-    <div className={styles.container}>
+    <Wrap className={styles.container} spacing={5}>
       {blogPosts.map((post) => {
         return (
-          <div key={post.id} className={styles.card}>
+          <WrapItem key={post.id} className={styles.card}>
             <VStack as="button" className={styles.card__container}>
               <h1>{post.title}</h1>
               <Link href={`/blog/${post.id}`}>Leia mais</Link>
@@ -16,9 +17,9 @@ export default function BlogPosts() {
             <div className={styles.footer}>
               <span>Data</span> - <span>Número de comentários</span>
             </div>
-          </div>
+          </WrapItem>
         );
       })}
-    </div>
+    </Wrap>
   );
 }
