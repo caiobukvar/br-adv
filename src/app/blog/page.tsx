f753cf264024f5f2899a6ea6f055b77420fb923a
Caio <bukvar2009@gmail.com>
2023-10-26 13:19:40 -0300

    fixing width for cards text @ blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,5 @@
-/* eslint-disable @next/next/no-async-client-component */
-"use client";
-import UnderConstruction from "@/components/UnderConstruction";
 import styles from "./page.module.css";
-import { useEffect, useState } from "react";
 import BlogPosts from "@/components/BlogPosts";
-// import BlogPosts from "@/components/BlogPosts";
 
 export default function Blog() {
   return (
