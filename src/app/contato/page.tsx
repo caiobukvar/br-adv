50495254ddf75dd91b1f64844528655e65f6067b
Caio <bukvar2009@gmail.com>
2023-10-30 11:57:47 -0300

    removendo metadata - useclient page

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -14,12 +14,6 @@ import {
 import { useTranslation } from "react-i18next";
 import styles from "./page.module.css";
 
-export async function generateMetadata() {
-  return {
-    title: "Contato",
-  };
-}
-
 export default function Contato() {
   const { t } = useTranslation();
 
