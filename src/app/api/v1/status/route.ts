377859f0125f4f86c202e74f796b656b1c91e257
Caio <bukvar2009@gmail.com>
2023-11-06 14:03:14 -0300

    instalando jest dev mode

--- src/app/api/v1/status/route.ts
+++ src/app/api/v1/status/route.ts
@@ -1,5 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 
+// ? for testing purpose:
+// ? curl http://localhost:3000/api/v1/status -v 
+
 // * GET SERVER STATUS
 export async function GET(req: NextRequest) {
   return NextResponse.json(
