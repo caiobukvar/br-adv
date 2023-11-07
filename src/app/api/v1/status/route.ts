329f2659d59b6b514162c57795d8b71a2ee05902
Caio <bukvar2009@gmail.com>
2023-11-07 13:11:46 -0300

    criando database.js

--- src/app/api/v1/status/route.ts
+++ src/app/api/v1/status/route.ts
@@ -1,16 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
+import database from "../../../../../infra/database.js";
 
-export async function GET(req: NextRequest) {
-  const teste = 1;
+export async function GET(req: NextRequest, res: NextResponse) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+
+  console.log(result);
 
   try {
+    console.log("Servidor está conectado");
     return NextResponse.json(
-      { valor: teste },
+      { valor: result },
       {
         status: 200,
       },
     );
   } catch (error) {
+    console.log("Servidor não está conectado");
     return NextResponse.json(
       { error: "ERROR" },
       {
