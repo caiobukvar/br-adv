09c2f6ca2970e2eae184ca85be51756545c17849
Caio <bukvar2009@gmail.com>
2023-12-12 14:08:33 -0300

    - se a resposta tem o status 200 - se tem uma data definida para updated_at (que é um valor q o teste recebe) - se tem uma data válida - se a versão do banco de dados é a 16.0 - se o numero max de conexoes do banco de dados eh 100 - se tem mais de uma conexao simultanea

--- src/app/tests/integration/api/v1/status/server-health.test.js
+++ src/app/tests/integration/api/v1/status/server-health.test.js
@@ -6,9 +6,16 @@ test("GET /api/v1/status returns 200", async () => {
   const responseBody = await response.json();
   expect(responseBody.updated_at).toBeDefined();
 
-  // caso não venha uma data, dá erro
+  // espera que realmente seja uma data
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
+  // espera que a versão do database seja igual a instalada inicialmente (caso tenha um  update, mudar a versão aqui)
   expect(responseBody.dependencies.database.version).toEqual("16.0");
+
+  // 100 é o default para um db LOCAL, tem que verificar quando em um banco 'real'
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+
+  // 1 para mostrar se tem alguma conexão 'vazando'
+  expect(responseBody.dependencies.database.open_connections).toEqual(1);
 });
