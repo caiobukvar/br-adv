5a5bd5ec4eaf78004cb450465ec2857f13e020f8
Caio <bukvar2009@gmail.com>
2023-12-13 15:29:56 -0300

    creating layout

---
+++ src/components/LoginScreen/index.tsx
@@ -0,0 +1,38 @@
+import {
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+  VStack,
+} from "@chakra-ui/react";
+
+export default function LoginScreen() {
+  return (
+    <VStack
+      p={10}
+      alignItems="center"
+      justifyContent="space-between"
+      borderRadius={7}
+      backgroundColor="#1c244b"
+      color="#fff"
+    >
+      <Heading marginBottom={5}>Login</Heading>
+
+      <FormControl>
+        <FormLabel fontWeight="bold">Nome de usuário</FormLabel>
+        <Input placeholder="Insira seu nome de usuário" name="username" />
+        <FormErrorMessage>Erro</FormErrorMessage>
+      </FormControl>
+
+      <FormControl>
+        <FormLabel fontWeight="bold">Senha</FormLabel>
+        <Input placeholder="Insira sua senha" name="password" />
+        <FormErrorMessage>Erro</FormErrorMessage>
+      </FormControl>
+
+      <Button marginTop={5}>Entrar</Button>
+    </VStack>
+  );
+}
