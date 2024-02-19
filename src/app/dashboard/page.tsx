b179946b89829a6d643160e7ba3e59ad1946d5a5
Caio <bukvar2009@gmail.com>
2024-02-19 10:31:40 -0300

    removing unused

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import LoginScreen from "@/components/LoginScreen";
+// import LoginScreen from "@/components/LoginScreen";
 import styles from "./page.module.css";
 // import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 // import { redirect } from "next/navigation";
