ff19a535ada9727f997bf567254be0d3fc735733
Caio <bukvar2009@gmail.com>
2024-02-19 10:33:52 -0300

    removing unused 2

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,29 +1,26 @@
 // import LoginScreen from "@/components/LoginScreen";
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
 // import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 // import { redirect } from "next/navigation";
-import RootLayout from "../layout";
+// import RootLayout from "../layout";
 
-const Dashboard = async () => {
-  // const { isAuthenticated } = getKindeServerSession();
-  // const isLoggedIn = await isAuthenticated();
+// const Dashboard = async () => {
+//   const { isAuthenticated } = getKindeServerSession();
+//   const isLoggedIn = await isAuthenticated();
 
-  // if (isLoggedIn) {
-  //   redirect("/dashboard/admin");
-  // }
+//   if (isLoggedIn) {
+//     redirect("/dashboard/admin");
+//   }
 
-  return (
-    <RootLayout isDashboardRoute={true}>
-      {/* {!isLoggedIn && (
-        <main className={styles.main}>
-          <LoginScreen />
-        </main>
-      )} */}
-      <main className={styles.main}>
-        <LoginScreen />
-      </main>
-    </RootLayout>
-  );
-};
+//   return (
+//     <RootLayout isDashboardRoute={true}>
+//       {!isLoggedIn && (
+//         <main className={styles.main}>
+//           <LoginScreen />
+//         </main>
+//       )}
+//     </RootLayout>
+//   );
+// };
 
-export default Dashboard;
+// export default Dashboard;
